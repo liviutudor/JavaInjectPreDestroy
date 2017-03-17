0915017451d163d5729cfe4c17a38811ac265f7e
Liviu Tudor <liviu.tudor@gmail.com>
2017-03-16 20:55:04 -0700

    skeleton

---
+++ src/main/java/liv/tudor/MainDriver.java
@@ -0,0 +1,16 @@
+package liv.tudor;
+
+/**
+ * Created by ltudor on 3/16/17.
+ */
+public class MainDriver {
+    public static void log(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        log("Starting program");
+
+        log("Ending program");
+    }
+}
